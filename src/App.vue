563e733ff326d3890e186d2afb6f26622f6f4a09
KEEN <kyh0927@live.co.kr>
2019-06-19 00:26:28 +0900

    vue re

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,21 @@
 
 <template>
   <div id="app" @contextmenu.prevent="ctxStop">
-    <p><input id="textInput" type="text" @keyup.13="deposit" v-model="amount"></p>
+    <p>
+      <input 
+        id="textInput"
+        type="text"
+        :style="style1"
+        @keyup.enter="deposit"
+        @mouseover.stop="overEvent"
+        @mouseout.stop="outEvent"
+        v-model="amount">
+    </p>
     <p v-show="isValidate === true">올바른 수를 입력하세요</p>
     <button id="deposit" @click="deposit()">예금</button>
     <button id="withdraw" @click="withdraw()">인출</button>
     <h3>계좌잔고 : {{balance}}</h3>
+    <button id="btn1" :style="[myColor, myLayout]">스타일 버튼</button>
   </div>
 </template>
 
@@ -18,10 +28,27 @@ export default {
     return {
       amount: 0,
       balance: 0,
-      isValidate: false
+      isValidate: false,
+      style1: {
+        border: "2px solid olive",
+        borderRadius: "4px",
+        backgroundColor: "aqua"
+      },
+      myColor: {
+        color: "blue"
+      },
+      myLayout: {
+        padding: "20px"
+      }
     }
   },
   methods: {
+    overEvent() {
+      this.style1.backgroundColor = "gray"
+    },
+    outEvent() {
+      this.style1.backgroundColor = "aqua"
+    },
     // deposit() {
     //   this.balance += parseInt(this.amount);
     //   this.amount = 0;
