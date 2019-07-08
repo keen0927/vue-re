c4944f6bbbc5406824a230598d3831722b16b2e4
keen <kyh0927@within.co.kr>
2019-07-08 16:03:05 +0900

    1

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,21 @@
         <binarySearch /> -->
 
 
-        <button name="tab-button" @click="contentShow('.div-wrap',1)" class="active">1</button>
-        <button name="tab-button" @click="contentShow('.div-wrap',2)">2</button>
-        <button name="tab-button" @click="contentShow('.div-wrap',3)">3</button>
-        <button name="tab-button" @click="contentShow('.div-wrap',4)">4</button>
+        <!-- <button name="tab-button" @click="contentShow('.content-area',1)" class="active">1</button>
+        <button name="tab-button" @click="contentShow('.content-area',2)">2</button>
+        <button name="tab-button" @click="contentShow('.content-area',3)">3</button>
+        <button name="tab-button" @click="contentShow('.content-area',4)">4</button> -->
+
+        <button name="tab-button" class="tab-button active" @click="contentShow(1)">1</button>
+        <button name="tab-button" class="tab-button" @click="contentShow(2)">2</button>
+        <button name="tab-button" class="tab-button" @click="contentShow(3)">3</button>
+        <button name="tab-button" class="tab-button" @click="contentShow(4)">4</button>
 
 
-        <div class="div-wrap active" style="display: block">1</div>
-        <div class="div-wrap">2</div>
-        <div class="div-wrap">3</div>
-        <div class="div-wrap">4</div>
+        <div class="content-area" v-show="isContentArea[0]">1</div>
+        <div class="content-area" v-show="isContentArea[1]">2</div>
+        <div class="content-area" v-show="isContentArea[2]">3</div>
+        <div class="content-area" v-show="isContentArea[3]">4</div>
     </div>
 </template>
 
@@ -29,7 +34,9 @@ import chatContainer from './containers/chatContainer';
         name: 'App',
         data() {
             return {
-        
+                isContentArea: [
+                    true, false, false, false
+                ]
             }
         },
         components: {
@@ -38,32 +45,54 @@ import chatContainer from './containers/chatContainer';
             // binarySearch
         },
         methods: {
-            contentShow(element, index) {
-                var elementName = element,
-                    targetElements = document.querySelectorAll(elementName),
-                    // buttonElement = document.querySelector('button.active'),
-                    button = document.querySelector('button'),
-                    buttonActiveElement = document.querySelector('button.active'),
-                    indexCalculate = index-1;
+            contentShow(index) {
+                var isContentAreaControl = [false, false, false, false];
+                isContentAreaControl[index-1] = true;
+                this.isContentArea = isContentAreaControl;
+                var button = document.querySelectorAll('.tab-button');
+                var buttonActiveElement = document.querySelector('button.active');
 
-                if (targetElements[indexCalculate].style.display !== "block") {
-                    targetElements.forEach(function(val){
-                        val.style.display = 'none';
-                    });
-                    targetElements[indexCalculate].style.display = 'block';
+                document.querySelectorAll('.tab-button').forEach(function(val){
                     buttonActiveElement.classList.remove('active');
-                    button[indexCalculate].classList.add('active');
-                    // buttonActiveElement[indexCalculate].classList.add('active');
-                    // console.log(document.querySelectorAll('name="tab-button"'));
-                };
+                    // document.querySelectorAll('.tab-button')[0].classList.add('active');
+                    button[index-1].classList.add('active');
+                });
+                
             }
+            // conten(element, index) {
+            //     va elementName = '.content-area',
+            //        = document.querySelectorAll(elementName),
+            //        = document.querySelector('button'),
+            //         buttonActiveElement = document.querySelector('button.active'),
+            //         indexCalculate = index-1;
+
+            //     if (targetElements[indexCalculate].offsetWidth > 0 || targetElements[indexCalculate].offsetheight > 0) {
+            //         return null;
+            //     } else {
+                    
+            //     }
+            //     // if (!targetElements[indexCalculate].offsetWidth > 0 || targetElements[indexCalculate].offsetheight > 0) {
+            //     //     console.log('11');
+            //     //     // targetElements.forEach(function(val){
+                        
+            //     //     // });
+            //     //     // targetElements[indexCalculate].style.display = 'block';
+            //     //     // buttonActiveElement.classList.remove('active');
+            //     //     // button[indexCalculate].classList.add('active');
+            //     // } else {
+            //     //     console.log('else');
+            //     // }
+            // }
         },
         
     }
 </script>
 
 <style lang="scss" scoped>
-.div-wrap {
+.content-area {
+    display: flex;
+}
+.content-area.hide {
     display: none;
 }
 button.active {
